dd13d48b018ec47fdcbe7530e79572b7f2adf54a
Lucas Bonny <lucasbonnyb8@gmail.com>
2024-09-23 02:36:55 -0300

    Tratamento de Exceção

---
+++ AtividadeException/src/model/exceptions/NotEnoughValue.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class NotEnoughValue extends Exception{
+	private static final long serialVersionUID = 1L;
+	public NotEnoughValue(String msg) {
+		super(msg);
+	}
+
+}
--- dias/0403/feltex-bot-telegram/src/main/java/br/com/feltex/bot/telegram/DadosBot.java
+++
@@ -1,6 +0,0 @@
-package br.com.feltex.bot.telegram;
-
-public class DadosBot {
-    public static final String BOT_TOKEN = "6246274732:AAHhvGknrlpHBDMeQgYkKVza4m8IjrSBCsw";
-    public static final String BOT_USER_NAME = "Tarefas Diárias - GuntherCloud";
-}
